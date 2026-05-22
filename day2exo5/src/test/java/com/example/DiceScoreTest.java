e473ebed1eb01582aa168b9575d239b4dbed0989
TMayess <mayess.tairi24@gmail.com>
2026-05-22 15:34:02 +0200

    add project, add DiceScoreTest

---
+++ day2exo5/src/test/java/com/example/DiceScoreTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiceScoreTest {
+    @Test
+    void testDeuxDesIdentiques() {
+        Ide deMock = mock(Ide.class);
+        when(deMock.getRoll()).thenReturn(4);
+
+        DiceScore diceScore = new DiceScore(deMock);
+        int result = diceScore.getScore();
+
+        assertEquals(18, result);
+    }
+
+    @Test
+    void testDoubleSix() {
+        Ide deMock = mock(Ide.class);
+        when(deMock.getRoll()).thenReturn(6);
+
+        DiceScore diceScore = new DiceScore(deMock);
+        int result = diceScore.getScore();
+
+        assertEquals(30, result);
+    }
+
+    @Test
+    void testDesQuelconques() {
+        Ide deMock = mock(Ide.class);
+        when(deMock.getRoll()).thenReturn(3, 5);
+
+        DiceScore diceScore = new DiceScore(deMock);
+        int result = diceScore.getScore();
+
+        assertEquals(5, result);
+    }
+}
